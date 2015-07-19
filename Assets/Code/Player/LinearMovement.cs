4aff6da3e2708590c0d5e7f9e8e7d2b9115a8508
Victor <vqholt@gmail.com>
2015-07-18 22:34:12 -0500

    Added jump to linear movement

--- Assets/Code/Player/LinearMovement.cs
+++ Assets/Code/Player/LinearMovement.cs
@@ -49,10 +49,30 @@ namespace WoVEssentials
 		/// <summary>
 		/// The player position.
 		/// </summary>
-		public Vector3 PlayerPos = Vector2.zero;
+		public Vector3 PlayerPos = Vector3.zero;
 
 		public float TimeReq = 0;
 		public float CurTime = 0;
+
+		/// <summary>
+		/// The hit ground threshold for testing if we're grounded.
+		/// </summary>
+		public float hitGroundThreshold = 0.2f;
+
+		/// <summary>
+		/// The jump speed.
+		/// Normalized value of 0 - 1.
+		/// </summary>
+		public float JumpForce = 250.0f;
+
+		/// <summary>
+		/// The jump force modifier.
+		/// </summary>
+		private float jumpForceModifier = 1.0f;
+		public float JumpForceModifier {
+			get { return jumpForceModifier; }
+			set { jumpForceModifier = value; }
+		}
 
 		/// <summary>
 		/// The enable automove flag.
@@ -82,6 +102,14 @@ namespace WoVEssentials
 		}
 
 		/// <summary>
+		/// The is jumping flag for the player.
+		/// </summary>
+		private bool isJumping = false;
+		public bool IsJumping {
+			get { return isJumping; }
+		}
+
+		/// <summary>
 		/// Awake this instance.
 		/// </summary>
 		public void Awake ()
@@ -105,11 +133,11 @@ namespace WoVEssentials
 		public void FixedUpdate ()
 		{
 			PlayerPos.y = transform.position.y;
-				
-			// Handle the movement.
+
 			if (Input.GetAxis ("Vertical") > 0) {
 				HandleVerticalMovement ();
 			}
+				
 			if (Input.GetAxis ("Vertical") < 0) {
 				HandleVerticalMovement (-1);
 			}
@@ -126,6 +154,11 @@ namespace WoVEssentials
 				HandleVerticalMovement (1, false);
 			}
 
+			// Handle the jumping movement.
+			if (IsJumping) {
+				HandleJumpMovement ();
+			}
+
 			// Slowly move camera to that new position.
 			transform.position = Vector3.Lerp (
 				transform.position, 
@@ -137,22 +170,43 @@ namespace WoVEssentials
 		}
 
 		/// <summary>
+		/// Determines whether this player is grounded.
+		/// 
+		/// TODO: This could be better...
+		/// </summary>
+		/// <returns><c>true</c> if this player is grounded; otherwise, <c>false</c>.</returns>
+		public bool IsGrounded ()
+		{
+			RaycastHit hit;
+			if (Physics.Raycast (
+				this.transform.position,
+				-Vector3.up,
+				out hit
+			)) {
+				if (hit.distance < hitGroundThreshold) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		/// <summary>
 		/// Handles the input keys.
 		/// </summary>
 		protected void HandleInputKeys ()
 		{
 			// Enable/disable automove.
-			if (Input.GetKeyUp (KeyCode.X)) {
+			if (Input.GetButtonUp ("AutoMove")) {
 				EnableAutomove = !EnableAutomove;
 			}
 
 			// Enable/disable sprint.
-			if (Input.GetKey (KeyCode.CapsLock)) {
+			if (Input.GetButtonUp ("SprintLock")) {
 				EnableSprintLock = !EnableSprintLock;
 			}
 
 			// Enable/disable sneak.
-			if (Input.GetKeyUp (KeyCode.LeftAlt)) {
+			if (Input.GetButtonUp ("Sneak")) {
 				EnableSneak = !enableSneak;
 
 				// Setup the player speed modifier.
@@ -166,6 +220,16 @@ namespace WoVEssentials
 				// TODO: When we have effect modifiers in place.
 				// SendMessage ("PlayerSpeedModifierUpdate");
 			}
+
+			// Set the IsJumping flag for the player.
+			// TODO: May want to check for flying mechanic later.
+			if (Input.GetButtonDown ("Jump") && !IsJumping) {
+				float jumpForce = JumpForce * JumpForceModifier;
+				isJumping = true;
+
+				Rigidbody rigidBody = GetComponent<Rigidbody> ();
+				rigidBody.AddForce (Vector3.up * jumpForce);
+			}
 		}
 
 		/// <summary>
@@ -174,11 +238,17 @@ namespace WoVEssentials
 		/// <param name="Direction">Direction.</param>
 		protected void HandleVerticalMovement (int Direction = 1, bool disableAutoMove = true)
 		{
+			// Exit if we're jumping.
+			// TODO: Do we want to handle cases where the player is going backwards?
+			if (IsJumping && Direction != 1) {
+				return;
+			}
+
 			// Set the new base player speed.
 			float newPlayerSpeed = PlayerSpeed * PlayerSpeedModifier;
 
 			// Modify the position.
-			if (!Input.GetButton ("Sprint") && !enableSprintLock) {
+			if ((!Input.GetButton ("Sprint") && !enableSprintLock) || IsJumping) {
 				PlayerPos += (transform.forward * newPlayerSpeed) * Direction;
 			} else {
 				PlayerPos += (transform.forward * (newPlayerSpeed * PlayerSprintModifier)) * Direction;
@@ -196,13 +266,37 @@ namespace WoVEssentials
 		/// <param name="Direction">Direction.</param>
 		protected void HandleHorizontalMovement (int Direction = 1)
 		{
+			// Exit if we're jumping.
+			if (IsJumping) {
+				return;
+			}
+
 			// Set the new base player speed.
 			float newPlayerSpeed = PlayerSpeed * PlayerSpeedModifier;
 
+			// Modify the position.
 			if (!Input.GetButton ("Sprint") && !enableSprintLock) {
 				PlayerPos += (transform.right * newPlayerSpeed) * Direction;
 			} else {
 				PlayerPos += (transform.right * (newPlayerSpeed * PlayerSprintModifier)) * Direction;
+			}
+
+			// Disable the automove.
+			EnableAutomove = false;
+		}
+
+		/// <summary>
+		/// Handles the jump movement.
+		/// </summary>
+		protected void HandleJumpMovement ()
+		{
+			// We should be grounded before attempting to
+			// jump.
+			Rigidbody rigidBody = GetComponent<Rigidbody> ();
+
+			if (IsGrounded () && rigidBody.velocity.y < 0.0f) {
+				isJumping = false;
+				return;
 			}
 
 			// Disable the automove.
